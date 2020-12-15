7b44805b91ed5a37727cfb75f1836cc7c5739366
Hustler <bridgeconnect11@gmail.com>
2020-12-15 21:47:53 +0800

    30percent

---
+++ app/src/main/java/com/example/i_knowledge/AlarmReceiver.java
@@ -0,0 +1,4 @@
+package com.example.i_knowledge;
+
+public class AlarmReceiver {
+}
---
+++ app/src/main/java/com/example/i_knowledge/covid19.java
@@ -0,0 +1,14 @@
+package com.example.i_knowledge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class covid19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid19);
+    }
+}
